62eca4438acf901eb73d4079236cb976ead47d19
DieGo <die_go_p@hotmail.com>
2020-08-02 21:53:19 -0500

    Test por dnone

--- src/routes/telephone.js
+++ src/routes/telephone.js
@@ -28,30 +28,5 @@ router.post('/:telephoneID', mAuth.tokenValidation, changeActivationTelephone);
 export default router;
 
 /*
-import { Router } from 'express';
-
-import mAuth from '../middlewares/authentication';
-
-const router = Router();
-
-import {createCity,
-        getCity,
-        getCities,
-        updateCity,
-        changeActivationCity,
-        deleteCity,
-        getCitiesProvince } from '../controllers/city.controller';
-
-// Routes without params
-router.post('/', [mAuth.tokenValidation, mAuth.adminValidation], createCity);
-router.get('/', mAuth.tokenValidation, getCities);
-
-// Routes with params
-router.get('/:cityID', mAuth.tokenValidation, getCity);
-router.put('/:cityID', [ mAuth.tokenValidation, mAuth.adminValidation ], updateCity);
-router.post('/:cityID', [ mAuth.tokenValidation, mAuth.adminValidation ], changeActivationCity);
-router.delete('/:cityID', [ mAuth.tokenValidation, mAuth.superAdminValidation ], deleteCity);
-router.get('/province/:provinceID', mAuth.tokenValidation, getCitiesProvince);
-
-export default router;
+    Test only for dnone branch
 */
