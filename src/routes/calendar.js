e070317bcef0cc96623e014ce999792c3f8451b3
Diego Peralta <die_go_p@hotmail.com>
2020-10-20 16:45:02 -0500

    Crono task added for automatic delete a date before finished day

--- src/routes/calendar.js
+++ src/routes/calendar.js
@@ -7,7 +7,9 @@ import {
     getAllCalendarDays,
     getCalendarDayByID,
     getCalendarDayByDate,
-    updateCalendarDay
+    updateCalendarDay,
+    changeToInactiveDay,
+    deleteYear,
 } from '../controllers/calendar.controller'
 
 const router = Router();
@@ -15,10 +17,12 @@ const router = Router();
 // Routes without params
 router.post('/', [mAuth.tokenValidation, mAuth.adminValidation], generateCalendar);
 router.get('/', mAuth.tokenValidation, getAllCalendarDays);
+router.put('/pastDay/', [mAuth.tokenValidation, mAuth.superAdminValidation], changeToInactiveDay);
 
 // Routes with params
 router.get('/:calendarID', mAuth.tokenValidation, getCalendarDayByID);
 router.get('/date/:date', mAuth.tokenValidation, getCalendarDayByDate);
 router.put('/:calendarID', [mAuth.tokenValidation, mAuth.adminValidation], updateCalendarDay);
+router.delete('/:year', [mAuth.tokenValidation, mAuth.superAdminValidation], deleteYear);
 
 export default router;
