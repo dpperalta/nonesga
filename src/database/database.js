4231908a704c243d3c66f0fcfaf62c232e13741a
dpperalta <die_go_p@hotmail.com>
2021-02-17 09:56:11 -0500

    Heroki postgress update v1

--- src/database/database.js
+++ src/database/database.js
@@ -1,14 +1,42 @@
 import Sequelize from 'sequelize';
 
-export const sequelize = new Sequelize(
+// PRODUCTION CONFIGURACIONB
+export const sequelize = new Sequelize('postgres://djisvgxhgdftyc:5bbc12f29c192c9d39bb13bd7159f7d7a2312a02a3c1dc74ab25c2ff89cfff36@ec2-34-237-89-96.compute-1.amazonaws.com:5432/dftjdgo4a61ifl', {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
+    pool: {
+        max: 5,
+        min: 0,
+        require: 30000,
+        idle: 10000
+    },
+    logging: false
+});
+
+/*  DEVELOPMENT CONFIGURATION */
+/*export const sequelize = new Sequelize(
     /*'nonesga', //esquema
     'postgres', //usuario
-    'admin', //contraseña*/
+    'admin', //contraseña
     process.env.DB_Q,
     process.env.DB_S,
     process.env.DB_R, {
         hots: process.env.DB_HOST,
         dialect: 'postgres',
+        protocol: 'postgres',
+        /*dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        },*/
+/*
         pool: {
             max: 5,
             min: 0,
@@ -17,7 +45,8 @@ export const sequelize = new Sequelize(
         },
         logging: false
     }
-);
+);*/
+
 
 async function connection() {
     try {
